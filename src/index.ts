8ee43ba65fe34d0385a9509acbb9e8650906a9d6
garvbahal <garvbahal75@gmail.com>
2026-03-26 17:36:41 +0530

    cors-health-status-added

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,20 @@ import cors from "cors";
 dotenv.config();
 const app = express();
 app.use(express.json());
-app.use(cors());
+
+const frontend_url = process.env.FRONTEND_URL;
+
+if (!frontend_url) {
+  throw new Error("frontend url is missing");
+}
+
+app.use(
+  cors({
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    origin: [frontend_url],
+    credentials: true,
+  }),
+);
 
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
@@ -12,10 +25,16 @@ import { connectDB } from "./config/mongo.js";
 
 app.use("/api/v1", userRouter);
 app.use("/api/v1", authRouter);
+app.get("/api/v1/health", (req, res) => {
+  return res.status(200).json({
+    success: true,
+    message: "Service working properly",
+  });
+});
 
 connectDB();
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
-    console.log(`App is running at ${PORT}`);
+  console.log(`App is running at ${PORT}`);
 });
