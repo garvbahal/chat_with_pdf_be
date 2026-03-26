434c447fd21f119103cf1e39f64f1fb1cd46817f
garvbahal <garvbahal75@gmail.com>
2026-03-26 17:52:04 +0530

    sendMail-issue-fixed

--- src/utils/sendmail.ts
+++ src/utils/sendmail.ts
@@ -1,24 +1,25 @@
 import nodemailer from "nodemailer";
 
 export async function sendMail(sendTo: string, subject: string, text: string) {
-    const transporter = nodemailer.createTransport({
-        service: "gmail",
-        host: process.env.MAIL_HOST,
-        auth: {
-            user: process.env.USER_GMAIL,
-            pass: process.env.USER_PASS,
-        },
-    });
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    port: 587,
+    secure: false,
+    auth: {
+      user: process.env.USER_GMAIL,
+      pass: process.env.USER_PASS,
+    },
+  });
 
-    const mailOptions = {
-        from: process.env.USER_GMAIL,
-        to: sendTo,
-        subject: subject,
-        html: text,
-    };
-    try {
-        await transporter.sendMail(mailOptions);
-    } catch (error) {
-        console.log("Error sending mail: ", error);
-    }
+  const mailOptions = {
+    from: process.env.USER_GMAIL,
+    to: sendTo,
+    subject: subject,
+    html: text,
+  };
+  try {
+    await transporter.sendMail(mailOptions);
+  } catch (error) {
+    console.log("Error sending mail: ", error);
+  }
 }
